d182c8ae3265113c0ad93c249e10e2a77fa0721a
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-02-22 16:41:47 +0100

    Added gitignore file, added gitkeep files for storing empty directories

--- src/api/app.py
+++ src/api/app.py
@@ -5,4 +5,4 @@ from api.config import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    run(f'{__name__}:app', host="0.0.0.0", debug=True, reload=True)
+    run(f'{__name__}:app', host='0.0.0.0', debug=True, reload=True)
