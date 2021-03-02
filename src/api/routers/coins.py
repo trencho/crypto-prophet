9179554f1b5130f65394dab50be577fdb4140978
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-03-02 10:18:37 +0100

    Added status codes when returning json response, added error messages for missing or invalid requests, added methods for forecasting coin values when accessing forecast endpoint

--- src/api/routers/coins.py
+++ src/api/routers/coins.py
@@ -1,6 +1,6 @@
 from os import path
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import ORJSONResponse
 from pandas import read_csv
@@ -12,7 +12,8 @@ coins_router = APIRouter(tags=['coins'])
 
 @coins_router.get('/coins/')
 async def fetch_coins():
-    return ORJSONResponse(jsonable_encoder(read_csv(path.join(DATA_EXTERNAL_PATH, 'coin_list.csv')).to_dict('records')))
+    return ORJSONResponse(jsonable_encoder(read_csv(path.join(DATA_EXTERNAL_PATH, 'coin_list.csv')).to_dict('records')),
+                          status_code=status.HTTP_200_OK)
 
 
 @coins_router.get('/coins/{coin_id}')
@@ -20,4 +21,7 @@ async def fetch_coin(coin_id: str = None):
     coin_list = read_csv(path.join(DATA_EXTERNAL_PATH, 'coin_list.csv')).to_dict('records')
     for coin in coin_list:
         if coin['id'] == coin_id:
-            return ORJSONResponse(jsonable_encoder(coin))
+            return ORJSONResponse(jsonable_encoder(coin), status_code=status.HTTP_200_OK)
+
+    message = 'Cannot return data because the coin is not found or is invalid.'
+    return ORJSONResponse(jsonable_encoder(f'error_message={message}'), status_code=status.HTTP_404_NOT_FOUND)
--- src/api/routers/forecast.py
+++ src/api/routers/forecast.py
@@ -1,16 +1,21 @@
+from datetime import datetime
 from os import path
 from pickle import load as pickle_load
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import ORJSONResponse
+from pandas import read_csv, to_datetime
 
-from definitions import MODELS_PATH
+from definitions import DATA_EXTERNAL_PATH, MODELS_PATH
 from modeling import train_coin_models
+from processing import closest_hour, current_hour, next_hour, recursive_forecast
 
 forecast_router = APIRouter(tags=['forecast'])
 
 
-@forecast_router.get('/forecast')
-async def forecast():
+@forecast_router.get('/forecast/{coin_id}')
+async def forecast(coin_id: str = None, timestamp: int = None):
     pass
 
 
@@ -29,3 +34,31 @@ def load_regression_model(coin):
         model_features = pickle_load(in_file)
 
     return model, model_features
+
+
+def forecast_city_sensor(coin, timestamp):
+    load_model = load_regression_model(coin)
+    if load_model is None:
+        return load_model
+
+    model, model_features = load_model
+
+    dataframe = read_csv(path.join(DATA_EXTERNAL_PATH, coin['symbol'], 'data.csv'))
+    dataframe.set_index(to_datetime(dataframe['time'] / 10 ** 3, unit='s'), inplace=True)
+
+    current_datetime = current_hour(datetime.now())
+    date_time = datetime.fromtimestamp(timestamp)
+    n_steps = (date_time - current_datetime).total_seconds() // 3600
+    return recursive_forecast(dataframe['value'], model, model_features, n_steps).iloc[-1]
+
+
+def retrieve_forecast_timestamp(timestamp):
+    next_hour_time = next_hour(datetime.now())
+    next_hour_timestamp = int(datetime.timestamp(next_hour_time))
+    timestamp = timestamp or next_hour_timestamp
+    if timestamp < next_hour_timestamp:
+        message = ('Cannot forecast pollutant because the timestamp is in the past. Send a GET request to the history '
+                   'endpoint for past values.')
+        return ORJSONResponse(jsonable_encoder(f'error_message={message}'), status_code=status.HTTP_400_BAD_REQUEST)
+
+    return closest_hour(datetime.fromtimestamp(timestamp)).timestamp()
