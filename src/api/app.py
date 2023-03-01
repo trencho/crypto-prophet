8938576fcc20994f40d713da07828827a6975d78
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-03-01 10:18:48 +0100

    Moved the initialization of the application, updated requirements.txt (orjson 3.8.6 -> 3.8.7, PyGithub 1.57 -> 1.58.0, scipy 1.10.0 -> 1.10.1)

--- src/api/app.py
+++ src/api/app.py
@@ -1,8 +1,28 @@
+from os import environ
+
+from fastapi import FastAPI
 from uvicorn import run
 
-from api.config import app
+from api.config import check_environment_variables, configure_logger, fetch_data, init_system_paths, register_routers, \
+    schedule_jobs
+from definitions import app_dev, app_env, app_prod
 
-app = app
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await init_system_paths()
+
+    if environ.get(app_env, app_dev) == app_prod:
+        await check_environment_variables()
+        await schedule_jobs()
+
+    await configure_logger()
+    await register_routers(app)
+
+    await fetch_data()
+
 
 if __name__ == '__main__':
     run('app:app', host='0.0.0.0', reload=True)
--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -1,25 +1,4 @@
-from os import environ
-
-from fastapi import FastAPI
-
-from definitions import app_dev, app_env, app_prod
 from .environment import check_environment_variables, fetch_data, init_system_paths
 from .logger import configure_logger
 from .routers import register_routers
-from .schedule import model_training, schedule_jobs
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup_event():
-    await init_system_paths()
-
-    if environ.get(app_env, app_dev) == app_prod:
-        await check_environment_variables()
-        await schedule_jobs()
-
-    await configure_logger()
-    await register_routers(app)
-
-    await fetch_data()
+from .schedule import schedule_jobs
